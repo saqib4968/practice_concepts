873b81800c53a9d8a5bd02e21fdc27ad3baa3988
saqib4968@gmail.com <saqib4968@gmail.com>
2023-04-26 23:27:36 +0530

    LCS with DPP

--- src/dynamic/programming/LongestCommonSubsequence.java
+++ src/dynamic/programming/LongestCommonSubsequence.java
@@ -6,6 +6,10 @@ public class LongestCommonSubsequence {
 
         System.out.println("Longest Common Subsequence: " +
                 getLCSLength("ABCBDAB".toCharArray(), 0, 7, "BDCABA".toCharArray(), 0, 6));
+
+        System.out.println("Longest Common Subsequence using DPP: " +
+                getLCSLengthUsingDPP("ABCBDAB".toCharArray(), 7, "BDCABA".toCharArray(), 6));
+
     }
 
     public static int getLCSLength(char[] x, int i, int m, char[] y, int j, int n){
@@ -17,5 +21,36 @@ public class LongestCommonSubsequence {
         }else{
             return Math.max(getLCSLength(x, i+1, m, y, j, n), getLCSLength(x, i, m, y, j+1, n));
         }
+    }
+
+    public static int getLCSLengthUsingDPP(char[] x, int m, char[] y, int n){
+
+        int [][] lCS = new int[1024][1024];
+        int i=0;
+        int j=0;
+        String longestCS = new String();
+        for(; i<=n; i++){
+            lCS[m][i] = 0;
+        }
+        for(; j<=m; j++){
+            lCS[j][n] = 0;
+        }
+        for(i=m-1; i>=0; i--){
+            for(j=n-1; j>=0; j--){
+                lCS[i][j] = lCS[i+1][j+1];
+                if(x[i] == y[j]){
+                    lCS[i][j]++;
+                    longestCS += x[i];
+                }
+                if(lCS[i][j+1] > lCS[i][j]){
+                    lCS[i][j] = lCS[i][j+1];
+                }
+                if(lCS[i+1][j] > lCS[i][j]){
+                    lCS[i][j] = lCS[i+1][j];
+                }
+            }
+        }
+        System.out.println(longestCS);
+        return lCS[0][0];
     }
 }
